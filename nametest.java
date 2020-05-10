f30df032eb2256bc3c6a1dc7dc95c09ff273c6ed
Vijay <Vijayaadharsh98@gmail.com>
2020-05-10 11:31:03 +0530

    New Changes

--- nametest.java
+++ nametest.java
@@ -1,10 +1,10 @@
 public class nametest{
 	
-	public static String arr[] = {"Vijay","Madhan","Ajay","Nimi","Viki"};
+	public static String arr[] = {"Vijay","Madhan","Ajay","Nimi","Viki","Yuvi"};
 	
 	public static void main(String[] args)
 	{
-		for(int i=0;i<5;i++)
+		for(int i=0;i<=5;i++)
 		{
 			System.out.println("Hello!!"+" "+arr[i]);
 		}
