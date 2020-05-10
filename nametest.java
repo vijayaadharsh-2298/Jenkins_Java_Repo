4ee7115f4501d9ade76f9deb6bc0a2841b62e0ce
Vijay <Vijayaadharsh98@gmail.com>
2020-05-10 11:14:29 +0530

    Added

---
+++ nametest.java
@@ -0,0 +1,13 @@
+public class nametest{
+	
+	public static String arr[] = {"Vijay","Madhan","Ajay","Nimi","Viki"};
+	
+	public static void main(String[] args)
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Hello!!"+" "+arr[i]);
+		}
+	}
+
+}
